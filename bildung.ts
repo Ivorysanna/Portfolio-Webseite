7d9a2f031803215c661d74e0a8d32876f89d02d8
Ivorysanna <violetta.pyralov@web.de>
2020-06-24 23:28:17 +0200

    Typescript dateien gelöscht

--- bildung.ts
+++
@@ -1,8 +0,0 @@
-namespace Lebenslauf{
-    interface Bildungsarten {
-        titel: string;
-        zweiterTitel: string;
-        dauer: number;
-    }
-    let bildungswegArray: Bildungsarten[] = JSON.parse(localStorage.getItem("bildungsarten")!);
-}
